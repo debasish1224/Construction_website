ba55f7bfa4c36ac452e9da5e48b9d735a858f2f6
DEBASISH <mrdebasishchatterjee213@gmail.com>
2024-07-16 16:37:59 +0530

    Updated admin page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ import { auth } from './firebase'; // Import auth from your Firebase configurati
 import UploadJob from './components/UploadJob';
 import UploadService from './components/UploadService';
 import UploadProduct from './components/UploadProduct';
+import UploadGallery from './components/UploadGallery';
+import UploadResearchDevelopment from './components/UploadResearchDevelopment';
+import DetailPage from './components/DetailPage';
+import DetailPage2 from './components/DetailPage2';
+import ViewBlog from './components/ViewBlog';
+import UploadAboutImage from './components/UploadAboutImage';
 
 
 function App() {
@@ -28,13 +34,19 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} /> 
+          <Route path="/product/:id" element={<DetailPage />} />
+          <Route path="/service/:id" element={<DetailPage2 />} />
           <Route path="/research-development" element={<ResearchDevelopment/>} />
+          <Route path="/viewBlog" element={<ViewBlog />} />
           <Route path="/carrerPage" element={<CareerPage/>}/>
           <Route path="/contact" element={<Contact />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/upload-job" element={<UploadJob />} />
           <Route path="/upload-service" element={<UploadService />} />
           <Route path="/upload-product" element={<UploadProduct />} />
+          <Route path="/upload-gallery" element={<UploadGallery />} />
+          <Route path="/upload-research-development" element={<UploadResearchDevelopment />} />
+          <Route path="/upload-aboutImage" element={<UploadAboutImage />} />
         </Routes>
         <Footer auth={auth} />
       </div>
